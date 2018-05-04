653947a36f9e1fa8137863a40e098d46d8647ca4
Mayowa Makinde <mayowa.makinde@andela.com>
2018-05-04 10:02:59 +0100

    Update products by Id

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,7 +1,7 @@
 import model from '../models';
 const Product = model.Product;
 const YardSale = model.YardSales;
-import { 
+import {
   sendMessage,
   sendData
 } from '../utilities';
@@ -21,28 +21,62 @@ module.exports = {
     const yardsaleId = parseInt(req.params.yardsaleId, 10);
     productObj.yardsaleId = yardsaleId;
     Product.findOne({
-      where: { 
+      where: {
         name: productObj.name,
         yardsaleId,
       },
       attributes: ['id']
     })
-    .then((existingProduct) => {
-      if(existingProduct) {
-        return sendMessage(res,
-          `A Product for already for this yardsale name title ${productObj.name}`,
-          400);
-      }
-      Product.create(productObj)
-      .then(newProduct => sendData(res, newProduct, 201, 'product'))
-      .catch((err) => {
-        // check for error when the yardsale does not exist
-        if ( err.message.includes('violates foreign key constraint')) {
-          return sendMessage(res, `There is no yardsale with the ID: ${yardsaleId}`, 400)
+      .then((existingProduct) => {
+        if (existingProduct) {
+          return sendMessage(res,
+            `A Product for already for this yardsale name title ${productObj.name}`,
+            400);
         }
-        return sendMessage(res, err.message, 500)
-      });
-    })
-    .catch(error => sendMessage(res, error.message, 500));
+        Product.create(productObj)
+          .then(newProduct => sendData(res, newProduct, 201, 'product'))
+          .catch((err) => {
+            // check for error when the yardsale does not exist
+            if (err.message.includes('violates foreign key constraint')) {
+              return sendMessage(res, `There is no yardsale with the ID: ${yardsaleId}`, 400)
+            }
+            return sendMessage(res, err.message, 500)
+          });
+      })
+      .catch(error => sendMessage(res, error.message, 500));
+  },
+
+    /**
+   * Updates content of product in the database
+   * Route: PUT: /products/:id
+   *
+   * @param {any} req
+   * @param {any} res
+   * @returns {Response} response object
+   */
+  updateProduct(req, res) {
+    return Product
+      .findOne({
+        where: {
+          id: req.params.productId
+        }
+      })
+      .then((product) => {
+        if (!product) {
+          return res
+            .status(404)
+            .send({ message: 'product Not Found' });
+        }
+        return product
+          .update({
+            name: req.body.name || product.name,
+            condition: req.body.condition || product.condition,
+            price: req.body.price || product.price
+          })
+          .then(updatedProduct => res.status(200)
+          .json({ message: 'Update Successful', updatedProduct }))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
   },
 }
