cc6612a58e762d0ba6e090aad62ceb3e4179f1c2
celelstine <okorocelestine@gmail.com>
2018-05-04 15:30:35 +0100

    add route to get user's cartitem

--- server/routes.js
+++ server/routes.js
@@ -7,36 +7,17 @@ const productController = Controllers.Product;
 const userController = Controllers.User;
 
 module.exports = (app) => {
-
-
-<<<<<<< HEAD
-  // login
-  app.post('/login',  userController.login);
-=======
-   // login
-   app.post('/login',  userController.login);
->>>>>>> add login route
+  app.post('/login', userController.login);
   // check for user session
-  // app.use(validateUser);
-
+  app.use(validateUser);
   app.get('/yardsales', yardsalesController.searchYardSale);
   app.get('/products', productController.searchProduct);
-  // admin routes start here 
-  app.post('/yardsales',  yardsalesController.createYardSale);
-  app.post('/yardsales/:yardsaleId/product', productController.createProduct);
-  app.put('/product/:productId', productController.updateProduct);
-<<<<<<< HEAD
-<<<<<<< HEAD
-  app.post('/product/:userId', productController.addtoCart);
-  // login
-  app.post('/login',  userController.login);
-=======
-  
->>>>>>> edit login
-  app.post('/yardsales/:yardsaleId/addtoCart/', productController.addtoCart);
-=======
   app.get('/yardsales/:yardsaleId/cartItems', productController.getCartItems);
   app.post('/yardsales/:yardsaleId/addtoCart', productController.addtoCart);
->>>>>>> add login route
+  // admin routes start here 
+  app.post('/yardsales', yardsalesController.createYardSale);
+  app.post('/yardsales/:yardsaleId/product', productController.createProduct);
+  app.put('/product/:productId', productController.updateProduct);
+  app.post('/yardsales/:yardsaleId/addtoCart/', productController.addtoCart);
 
 }
