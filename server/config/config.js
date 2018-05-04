100fed61662cca38337b0efaf674c54e16faae2c
Esther Falayi <esther.falayi@gmail.com>
2018-05-04 12:05:22 +0100

    - edit database config file - add .env.sample file

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,11 @@
-{
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
   "development": {
-    "username": "andeladeveloper",
-    "password": "smilesh2o",
-    "database": "yardSale-dev",
+    "username": process.env.DB_USER_NAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
